8567d2aab82a8f3350e2b6a6f42d0754d3c52489
garuka-satharasinghe <142331771+garuka-satharasinghe@users.noreply.github.com>
2024-05-15 15:39:52 +0530

    projects

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>{{ name }}</h1>
+    <ul>
+      <li v-for="project in projects" :key="project.name">
+        <h2>{{ project.name }}</h2>
+        <p>{{ project.description }}</p>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup>
+const name='Garuka Satharasinghe';
+const {data:projects,pending,error}=useFetch('http://localhost:5000/projects')
+</script>
+
+<style>
+</style>
