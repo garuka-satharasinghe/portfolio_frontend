246ce5c2da8e7fc6354a9c8efa79d1fb56e16057
garuka-satharasinghe <142331771+garuka-satharasinghe@users.noreply.github.com>
2024-05-15 16:24:24 +0530

    Projects and blogs

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <h1>{{ name }}</h1>
+  <h1>{{ name }}</h1>
+  
+    <div v-if="pending">Loading Projects...</div>
+    <div>
+    <h2>Projects</h2>
     <ul>
       <li v-for="project in projects" :key="project.name">
         <h2>{{ project.name }}</h2>
@@ -8,11 +11,25 @@
       </li>
     </ul>
   </div>
+
+  <div v-if="pending1">Loading Blogs...</div>
+  <div>
+    <h2>Blogs</h2>
+    <ul>
+      <li v-for="blog in blogs" :key="blog.name">
+        <h2>{{ blog.title }}</h2>
+        <p>{{ blog.content }}</p>
+      </li>
+    </ul>
+  </div>
+
+  
 </template>
 
 <script setup>
 const name='Garuka Satharasinghe';
 const {data:projects,pending,error}=useFetch('http://localhost:5000/projects')
+const {data:blogs,pending1,error1}=useFetch('http://localhost:5000/blogs')
 </script>
 
 <style>
