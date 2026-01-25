eab23144128eba92aba77d7d1bf94035b7d3e077
garuka-satharasinghe <methman2002@gmail.com>
2026-01-25 18:56:16 +0530

    Fix Google site verification meta tag content format

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@ useHead({
   meta: [
     {
       name: 'google-site-verification',
-      content: '<meta name="google-site-verification" content="0O8b55vmsOOU6b3uBgSy2VxIklcU_-pDHXctNOrL-GE" />'
+      content: '0O8b55vmsOOU6b3uBgSy2VxIklcU_-pDHXctNOrL-GE'
     }
   ]
 })
