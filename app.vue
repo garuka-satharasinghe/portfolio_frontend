64a908f92a539be20acc392a0660d1b55f599b81
garuka-satharasinghe <methman2002@gmail.com>
2026-01-25 18:53:00 +0530

    Add Google site verification meta tag for SEO

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+useHead({
+  meta: [
+    {
+      name: 'google-site-verification',
+      content: '<meta name="google-site-verification" content="0O8b55vmsOOU6b3uBgSy2VxIklcU_-pDHXctNOrL-GE" />'
+    }
+  ]
+})
+</script>
+
 <template>
   <div>
     <LoadingScreen />
